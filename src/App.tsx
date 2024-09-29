e7b4d99edd3c34cade90bcf51472af3b271dfc62
Sernak94 <sernak94@yandex.ru>
2024-09-29 13:23:48 +0300

    <p>Sergey privet</p>36

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
-
+import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
+import { HW4 } from './HW/4/HW4';
 
 function App() {
   return (
@@ -8,7 +11,7 @@ function App() {
       {/*<HW2 />*/}
       {/*<HW3 />*/}
       {/*<HW4 />*/}
-      <p>Sergey poka/</p>
+      <p>Sergey pok5/</p>
     </div>
   );
 }
